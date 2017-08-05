1836171875ab05a11af04948421fd53ee704ed25
Alexander <lss1ash@rambler.ru>
2017-08-06 02:57:34 +1100

    Метод post + JS начат

--- js/main.js
+++ js/main.js
@@ -2,5 +2,17 @@
 
 (function () {
 
+  var inputField = {
+    fio: document.getElementById('fio'),
+    email: document.getElementById('email'),
+    phone: document.getElementById('phone')
+  };
+  var submitButton = document.getElementById('submitButton');
+  var resultContainer = document.getElementById('resultContainer');
+
+
+
+  submitButton.addEventListener('click', validateForm);
+  submitButton.addEventListener('keydown', submitKeydownHandler);
 
 }());
