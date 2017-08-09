21f1500ce97f1a83f42b6bac2656633fbd339a1d
Alexander <lss1ash@rambler.ru>
2017-08-09 22:54:31 +1100

    Вся логика в index.js - ок, объединю обратно

--- index.js
+++ index.js
@@ -3,12 +3,26 @@
 (function (app) {
 
   var ERROR_CLASS = 'error';
+  var FIO_WORDS_COUNT = 3;
+  var PHONE_SUM = 30;
+
+  var errorFields;
+  app = {};
 
   // var submitButton = myForm.querySelector('#submitButton');
   // var resultContainer = myForm.querySelector('#resultContainer');
 
+  var myForm = document.getElementById('myForm');
+  var inputFields = {
+    fio: myForm.querySelector('#fio'),
+    email: myForm.querySelector('#email'),
+    phone: myForm.querySelector('#phone')
+  };
+
+// Визуальное выделение невалидных полей //
+
   var clearInvalid = function () {
-    [app.inputFields.fio, app.inputFields.email, app.inputFields.phone].forEach(function (field) {
+    [inputFields.fio, inputFields.email, inputFields.phone].forEach(function (field) {
       if (field.classList.contains(ERROR_CLASS)) {
         field.classList.remove(ERROR_CLASS);
       }
@@ -21,7 +35,54 @@
     });
   };
 
-  var submitForm = function (e) {
+// Валидация
+
+  var isBasicallyValid = function (input) {
+    if (!input.validity.valid) {
+      errorFields.push(input.name);
+      return false;
+    }
+    return true;
+  };
+
+  var isSpeciallyValidFio = function () {
+    var fioWords = inputFields.fio.value.trim().split(' ').filter(function (word) {
+      return word.length > 0;
+    }).length;
+    if (fioWords !== FIO_WORDS_COUNT) {
+      errorFields.push(inputFields.fio.name);
+      return false;
+    }
+    return true;
+  };
+
+  var isSpeciallyValidPhone = function () {
+    var phoneSum = inputFields.phone.value.replace(/\D/g, '').split('').reduce(function (sum, value) {
+      return +sum + +value;
+    }, 0);
+    if (phoneSum > PHONE_SUM) {
+      errorFields.push(inputFields.phone.name);
+      return false;
+    }
+    return true;
+  };
+
+// validate в шлобальную область
+
+  app.validate = function () {
+    errorFields = [];
+
+    var fioValid = isBasicallyValid(inputFields.fio) && isSpeciallyValidFio();
+    var emailValid = isBasicallyValid(inputFields.email);
+    var phoneValid = isBasicallyValid(inputFields.phone) && isSpeciallyValidPhone();
+
+    return {
+      isValid: fioValid && emailValid && phoneValid,
+      errorFields: errorFields
+    };
+  };
+
+  app.submit = function (e) {
     e.preventDefault();
     clearInvalid();
     var validity = app.validate();
@@ -30,7 +91,6 @@
     }
   };
 
-  var myForm = document.getElementById('myForm');
-  myForm.addEventListener('submit', submitForm);
+  myForm.addEventListener('submit', app.submit);
 
 }(window.MyForm));
